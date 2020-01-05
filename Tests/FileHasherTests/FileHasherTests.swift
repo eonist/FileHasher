2234f9e4873b55633cdc256142103bb165d019ee
Eonist <30n1st@gmail.com>
2020-01-05 19:31:55 +0100

    Add file-based unit-test

--- Tests/FileHasherTests/FileHasherTests.swift
+++ Tests/FileHasherTests/FileHasherTests.swift
@@ -1,5 +1,6 @@
 import XCTest
 import FileHasher
+import ResourceHelper
 
 class FileHasherTests: XCTestCase {
    override func setUp() {
@@ -9,8 +10,8 @@ class FileHasherTests: XCTestCase {
       super.tearDown()
    }
    func testExample() {
-      XCTAssertEqual("Hello, World!", "Hello, World!")
-      // add NetworkSugar, download the mp3 to a temp folder and check the hash, because spm and bundle 🤷
+      //XCTAssertEqual("Hello, World!", "Hello, World!")
+      XCTAssertTrue(hashTest())
    }
    func testPerformanceExample() {
       self.measure { }
@@ -21,13 +22,16 @@ extension FileHasherTests {
     * hashTest
     * - Fixme: ⚠️️ maybe compare txt byte for byte
     */
-   public static func hashTest() -> String? {
+   func hashTest() -> Bool {
       //      let fromFilePath:String = NSString(string: "~/Desktop/demo.mp3").expandingTildeInPath
-      let fromFilePath: String = "\(Bundle.main.resourcePath!)/temp.bundle/demo.mp3"//NSString(string: "~/Desktop/complexContent.txt").expandingTildeInPath // let fromFilePath: String = NSString(string: "~/Desktop/tester.txt").expandingTildeInPath
-      //      let fromFilePath:String = "/var/folders/j_/2ghwmc6d6555x9_zn680jgl40000gn/T/test.txt"
-      /*guard */let url: URL = .init(fileURLWithPath: fromFilePath)// else { Swift.print("err url"); return }
-      guard let trucatedFileHash: String = FileHashUtil.truncatedHash(url: url) else { Swift.print("err"); return nil }
+      let url: URL = ResourceHelper.projectRootURL(projectRef: #file, fileName: "temp.bundle/demo.mp3")//.path
+      //let fromFilePath: String = "\(Bundle.main.resourcePath!)/temp.bundle/demo.mp3"//NSString(string: "~/Desktop/complexContent.txt").expandingTildeInPath // let fromFilePath: String = NSString(string: "~/Desktop/tester.txt").expandingTildeInPath
+      //let fromFilePath:String = "/var/folders/j_/2ghwmc6d6555x9_zn680jgl40000gn/T/test.txt"
+//      /*guard */let url: URL = .init(fileURLWithPath: fromFilePath)// else { Swift.print("err url"); return }
+      guard let trucatedFileHash: String = FileHashUtil.truncatedHash(url: url) else { Swift.print("err"); return false }
       //      Swift.print("hash: \(trucatedFileHash)")
-      return trucatedFileHash // 5949001440284795020
+      let integrity: Bool = trucatedFileHash == "5949001440284795020"
+      Swift.print("Test done: \(integrity ? "✅" : "🚫")")
+      return integrity
    }
 }
